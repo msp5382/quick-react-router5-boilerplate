c5dc36bd90c31412d56cdc6c83572035bbfad5c7
matcha <meen.sankaew@gmail.com>
2020-09-20 08:52:15 +0700

    forgot this

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useRoute } from "react-router5";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
+
+export default (props) => {
+  const { router } = useRoute();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <div className="row justify-content-center">
+          <div className="col-lg-5 col-md-11 col-sm-11">
+            <div>{router.getState().component}</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
-
-export default App;
+};
